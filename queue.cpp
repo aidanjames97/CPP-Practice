658c357aa31046d2ef47fdf7071c1d81bc0606e4
Aidan James <ajame23@uwo.ca>
2024-02-23 16:17:03 -0500

    remove and insert fixed, implemented working display

--- queue.cpp
+++ queue.cpp
@@ -29,21 +29,26 @@ class queue {
                 tail = newNode;
             }
 
-            // queue not empty
-            newNode->next = tail;
+            // check for one element
+            if(head->prev == nullptr) {
+                head->prev = newNode;
+            }
+
+            // inserting
             tail->prev = newNode;
+            newNode->next = tail;
+            newNode->prev = nullptr;
             tail = newNode;
         }
 
+        // remove and return the next element in the queue
         int remove() {
             // check if queue empty
             if(head == nullptr) {
-                cout << "Queue is empty, cannot remove anything\n";
                 throw runtime_error("Empty Queue");
             }
             // check if only one node in queue
             if(head->prev == nullptr) {
-                cout << "one elem queue" << endl;
                 int d = head->data; // get data and store
                 head = nullptr; // head points to null
                 tail = nullptr; // tail points to null
@@ -55,6 +60,23 @@ class queue {
             head->next = nullptr; // set head's next ptr to null
             return tmp->data; // return data
         }
+
+        // display all elements in queue
+        void display() {
+            queue temp;
+            cout << " HEAD" << endl;
+            while(head != nullptr) {
+                int tmp = remove();
+                temp.insert(tmp);
+                cout << "| " << tmp << " |" << endl;
+            }
+            cout << " TAIL" << endl;
+
+            // set original queue to temp queue
+            while(temp.head != nullptr) {
+                insert(temp.remove());
+            }
+        }   
 };
 
 int main() {
@@ -62,16 +84,28 @@ int main() {
 
     queue q;
 
+    cout << "queue should be empty" << endl;
+
     try {
         // test empty remove
         q.remove();
     } catch (const exception& e) {
-        cout << e.what() << endl;
+        cout << e.what() << "\n" << endl;
     }
+
+    cout << "remove should be 6" << endl;
 
     // test insert and remove with one item
     q.insert(6);
     cout << q.remove() << endl;
+
+    try {
+        // test remove then empty queue
+        cout << "should be Empty Queue" << endl;
+        q.remove();
+    } catch (const exception& e) {
+        cout << e.what() << "\n" << endl;
+    }
 
     // test inserting many elements
     q.insert(1);
@@ -80,7 +114,12 @@ int main() {
     q.insert(4);
     q.insert(5);
 
+    cout << "should be 1,2,3,4,5" << endl;
+    q.display();
+
+    cout << "\nshould be 1 then 2" << endl;
+
     cout << q.remove() << endl;
-    cout << q.remove() << endl;
+    cout << q.remove() << "\n" << endl;
 
 }
